343b26e386cb926c73b54e56ed5b4df0e3952f6d
helciofreire <helcio1.freire@gmail.com>
2025-11-07 17:15:47 -0300

    Health2

--- server.js
+++ server.js
@@ -146,12 +146,6 @@ app.get("/health", (req, res) => {
   return res.status(200).json({ ok: true, status: "online" });
 });
 
-// -----------------------------------------------------
-// Iniciar servidor
-// -----------------------------------------------------
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor online na porta ${PORT}`));
-
 
 // -----------------------------------------------------
 // Iniciar servidor
