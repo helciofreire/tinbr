754aac6dc67b06c0c99ebc13e3b973e485ae6e08
Helcio Freire do Carmo <helcio1.freire@gmail.com>
2026-09-01 17:25:07 -0300

    newprop2

--- server.js
+++ server.js
@@ -662,12 +662,6 @@ app.get("/proprietarios/bloqueados-por-responsavel/:cpfresp", async (req, res) =
 app.post("/proprietarios", async (req, res) => {
   try {
     const dados = req.body;
-
-	console.log("================================");
-    	console.log("📥 POST /proprietarios");
-    	console.log("🏢 companyType recebido:", dados.companyType);
-    	console.log("📦 BODY RECEBIDO:", dados);
-    	console.log("================================");
     
     // ✅ Validação obrigatória
     if (!dados.cliente_id) {
